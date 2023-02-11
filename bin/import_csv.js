dcf6b86c923991d68663af9d8452ea5d53d22493
happo31 <happo31@gmail.com>
2023-02-12 01:35:39 +0900

    feat: csv からDBを作成するスクリプト作成

---
+++ bin/import_csv.js
@@ -0,0 +1,87 @@
+const sqlite3 = require("sqlite3").verbose();
+const csvParse = require("csv-parse");
+const dotenv = require("dotenv");
+const { readFile } = require("fs/promises");
+const { Database } = require("sqlite3");
+
+(async () => {
+  const stage = process.env.STAGE;
+
+  dotenv.config({
+    path: stage ? `.env.${stage}` : ".env"
+  });
+
+  const dbName = process.env["SQLITE_DB_NAME"];
+
+  const createTableSQLs = (await readFile("./bin/create_table.sql")).toString().split("\n");
+  const db = new sqlite3.Database(dbName);
+
+  createTableSQLs.forEach(sql => {
+    db.run(sql, (_err) => {
+    });
+  })
+
+  await imporFromtCsv(db, "arknights", "./assets/arknights.csv");
+  await imporFromtCsv(db, "bluearchive", "./assets/bluearchive.csv");
+  await imporFromtCsv(db, "imas_cinderella", "./assets/imas_cinderella.csv");
+
+
+  db.close();
+})();
+
+/**
+ * 
+ * @param {Database} db 
+ * @param {string} tableName 
+ * @param {string} csvPath 
+ * @returns
+ */
+async function imporFromtCsv(db, tableName, csvPath) {
+  return new Promise(async (resolve, reject) => {
+    const csv = await readFile(csvPath);
+    const parser = csvParse.parse({ delimiter: ",", columns: false });
+    /**
+     * @type string[][]
+     **/
+    let records = [];
+
+    parser.on("readable", () => {
+      let record;
+      while ((record = parser.read()) !== null) {
+        records.push(record);
+      }
+      // カラム名の行を削除
+      records = records.slice(1);
+
+      try {
+        db.serialize(() => {
+          const state = db.prepare(`insert or ignore into ${tableName} (cv_name, cv_name_read, chara) values (?, ?, ?)`);
+          // placeholder への適用
+          records.forEach(row => state.run([row[0], row[1], row[2]]));
+          state.finalize(function (err) {
+            if (err) {
+              console.error("db.run", err);
+              reject(err);
+              return;
+            }
+            resolve();
+          });
+        });
+      } catch (e) {
+        console.error(e);
+        resolve();
+      }
+    });
+
+    parser.on("end", () => {
+
+    });
+
+    parser.on("error", (err) => {
+      reject(err);
+    });
+
+    parser.write(csv);
+
+  });
+}
