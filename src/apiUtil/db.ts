01817e4a3be6a081257c84c4950bb886ec795105
happo31 <happo31@gmail.com>
2023-02-24 01:55:44 +0900

    fix: 調査

--- src/apiUtil/db.ts
+++ src/apiUtil/db.ts
@@ -1,6 +1,5 @@
 import * as sqlite3 from "sqlite3";
 import path from "path";
-import { readdir, readdirSync } from "fs";
 
 const dbName = process.env["SQLITE_DB_NAME"] ?? "cv.db";
 let db: sqlite3.Database;
@@ -24,15 +23,12 @@ export default async function all<T = any>(
 }
 
 function initDB() {
-  if (db == null) {
-    throw new Error(`dirs: ${readdirSync(process.cwd()).join(" ")}`);
-    if (process.env.NODE_ENV === "development") {
-      const sqlite = sqlite3.verbose();
-      db = new sqlite.Database(path.join(process.cwd(), dbName));
-      db.on("trace", (sql) => console.log(sql));
-    } else {
-      db = new sqlite3.Database(path.join(process.cwd(), dbName));
-    }
-    db.on("error", (err) => console.error(err));
+  if (process.env.NODE_ENV === "development") {
+    const sqlite = sqlite3.verbose();
+    db = new sqlite.Database(path.join(process.cwd(), dbName));
+    db.on("trace", (sql) => console.log(sql));
+  } else {
+    db = new sqlite3.Database(path.join(process.cwd(), dbName));
   }
+  db.on("error", (err) => console.error(err));
 }
