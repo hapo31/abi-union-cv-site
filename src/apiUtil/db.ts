52fb9ac12faa7bf193d5d7a2bae0511fa2df0ee3
happo31 <happo31@gmail.com>
2023-02-24 01:31:39 +0900

    fix: verbose は development でだけ有効

--- src/apiUtil/db.ts
+++ src/apiUtil/db.ts
@@ -1,6 +1,5 @@
 import * as sqlite3 from "sqlite3";
 import path from "path";
-const sqlite = sqlite3.verbose();
 
 const dbName = process.env["SQLITE_DB_NAME"] ?? "cv.db";
 let db: sqlite3.Database;
@@ -25,9 +24,13 @@ export default async function all<T = any>(
 
 function initDB() {
   if (db == null) {
-    db = new sqlite.Database(path.join(process.cwd(), dbName));
+    console.log("cwd: ", process.cwd());
     if (process.env.NODE_ENV === "development") {
+      const sqlite = sqlite3.verbose();
+      db = new sqlite.Database(path.join(process.cwd(), dbName));
       db.on("trace", (sql) => console.log(sql));
+    } else {
+      db = new sqlite3.Database(path.join(process.cwd(), dbName));
     }
     db.on("error", (err) => console.error(err));
   }
