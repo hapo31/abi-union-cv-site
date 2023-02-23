4f6e6cddcf9911dc41c65f3d4c5d48765dfcc615
happo31 <happo31@gmail.com>
2023-02-24 01:48:58 +0900

    また調査

--- src/apiUtil/db.ts
+++ src/apiUtil/db.ts
@@ -1,5 +1,6 @@
 import * as sqlite3 from "sqlite3";
 import path from "path";
+import { readdir, readdirSync } from "fs";
 
 const dbName = process.env["SQLITE_DB_NAME"] ?? "cv.db";
 let db: sqlite3.Database;
@@ -24,7 +25,7 @@ export default async function all<T = any>(
 
 function initDB() {
   if (db == null) {
-    console.log("cwd: ", process.cwd());
+    throw new Error(`dirs: ${readdirSync(process.cwd()).join(" ")}`);
     if (process.env.NODE_ENV === "development") {
       const sqlite = sqlite3.verbose();
       db = new sqlite.Database(path.join(process.cwd(), dbName));
