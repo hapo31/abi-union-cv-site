f3107500b244c33411df57c53f1ad86220ed9fda
happo31 <happo31@gmail.com>
2023-02-24 12:36:48 +0900

    fix: copy-webpack-plugin を使う

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
+const Copy = require('copy-webpack-plugin')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "dist"
+  distDir: "dist",
+  webpack: (config) => {
+    config.plugins.push(new Copy({
+      patterns: [
+        process.env['SQLITE_DB_NAME']
+      ]
+    }));
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
