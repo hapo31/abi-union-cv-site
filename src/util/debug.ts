d6f34278086b0d2a11546248170ca3cbc58367f8
happo31 <happo31@gmail.com>
2023-02-24 14:49:08 +0900

    fix

--- src/util/debug.ts
+++ src/util/debug.ts
@@ -5,21 +5,35 @@ export async function outputDirTree(
   parent: string,
   ignoreDirs: string[] = ["node_modules", "etc", "usr", "dev"]
 ): Promise<Record<string, string[] | unknown>> {
-  const dirs = await readdir(parent);
+  let dirs: string[];
+  try {
+    dirs = await readdir(parent);
+  } catch {
+    return { [parent]: "(permission_denied)" };
+  }
   const fileStats = await Promise.all(
     dirs
       .filter(
         (dir) => !dir.startsWith(".") && (!ignoreDirs?.includes(dir) ?? true)
       )
-      .map(async (file) => ({
-        file,
-        stat: await stat(path.join(parent, file)),
-      }))
+      .map(async (file) => {
+        try {
+          return {
+            file,
+            stat: await stat(path.join(parent, file)),
+          };
+        } catch {
+          return {
+            file,
+            stat: null,
+          };
+        }
+      })
   );
 
   const result = await Promise.all(
     fileStats.map(async ({ file, stat }) => {
-      if (stat.isDirectory()) {
+      if (stat == null || stat.isDirectory()) {
         return await outputDirTree(path.join(parent, file), ignoreDirs);
       } else {
         return file;
