3e9654069daa314fa3dac110b21a47c5a687246a
happo31 <happo31@gmail.com>
2023-03-04 00:11:40 +0900

    Revert "fix: ファイルアクセスには process.cwd() と path.join する"
    This reverts commit 300718607b7f12e7215385d00764b51e058e7b2b.

--- src/apiUtil/db.ts
+++ src/apiUtil/db.ts
@@ -1,15 +1,18 @@
 import * as sqlite3 from "sqlite3";
-import path from "path";
 const sqlite = sqlite3.verbose();
 
 const dbName = process.env["SQLITE_DB_NAME"] ?? "cv.db";
-let db: sqlite3.Database;
+const db = new sqlite.Database(dbName);
+
+if (process.env.NODE_ENV === "development") {
+  db.on("trace", (sql) => console.log(sql));
+}
+db.on("error", (err) => console.error(err));
 
 export default async function all<T = any>(
   sql: string,
   params: unknown = []
 ): Promise<T[]> {
-  initDB();
   return new Promise((resolve, reject) => {
     db.serialize(() => {
       db.all(sql, params, (err, row) => {
@@ -21,14 +24,4 @@ export default async function all<T = any>(
       });
     });
   });
-}
-
-function initDB() {
-  if (db == null) {
-    db = new sqlite.Database(path.join(process.cwd(), dbName));
-    if (process.env.NODE_ENV === "development") {
-      db.on("trace", (sql) => console.log(sql));
-    }
-    db.on("error", (err) => console.error(err));
-  }
 }
