0ca469f82da5498bc8d5abd2b93cb9bcb3ffa347
happo31 <happo31@gmail.com>
2023-03-04 00:11:40 +0900

    Revert "fix: verbose は development でだけ有効"
    This reverts commit 52fb9ac12faa7bf193d5d7a2bae0511fa2df0ee3.

--- src/apiUtil/db.ts
+++ src/apiUtil/db.ts
@@ -1,5 +1,6 @@
 import * as sqlite3 from "sqlite3";
 import path from "path";
+const sqlite = sqlite3.verbose();
 
 const dbName = process.env["SQLITE_DB_NAME"] ?? "cv.db";
 let db: sqlite3.Database;
@@ -24,13 +25,9 @@ export default async function all<T = any>(
 
 function initDB() {
   if (db == null) {
-    console.log("cwd: ", process.cwd());
+    db = new sqlite.Database(path.join(process.cwd(), dbName));
     if (process.env.NODE_ENV === "development") {
-      const sqlite = sqlite3.verbose();
-      db = new sqlite.Database(path.join(process.cwd(), dbName));
       db.on("trace", (sql) => console.log(sql));
-    } else {
-      db = new sqlite3.Database(path.join(process.cwd(), dbName));
     }
     db.on("error", (err) => console.error(err));
   }
