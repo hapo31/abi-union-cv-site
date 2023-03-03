4c00815ca74ce45f0403252e970f752e3cc45ea6
happo31 <happo31@gmail.com>
2023-03-04 00:11:19 +0900

    Revert "fix"
    This reverts commit d6f34278086b0d2a11546248170ca3cbc58367f8.

--- src/util/debug.ts
+++ src/util/debug.ts
@@ -5,35 +5,21 @@ export async function outputDirTree(
   parent: string,
   ignoreDirs: string[] = ["node_modules", "etc", "usr", "dev"]
 ): Promise<Record<string, string[] | unknown>> {
-  let dirs: string[];
-  try {
-    dirs = await readdir(parent);
-  } catch {
-    return { [parent]: "(permission_denied)" };
-  }
+  const dirs = await readdir(parent);
   const fileStats = await Promise.all(
     dirs
       .filter(
         (dir) => !dir.startsWith(".") && (!ignoreDirs?.includes(dir) ?? true)
       )
-      .map(async (file) => {
-        try {
-          return {
-            file,
-            stat: await stat(path.join(parent, file)),
-          };
-        } catch {
-          return {
-            file,
-            stat: null,
-          };
-        }
-      })
+      .map(async (file) => ({
+        file,
+        stat: await stat(path.join(parent, file)),
+      }))
   );
 
   const result = await Promise.all(
     fileStats.map(async ({ file, stat }) => {
-      if (stat == null || stat.isDirectory()) {
+      if (stat.isDirectory()) {
         return await outputDirTree(path.join(parent, file), ignoreDirs);
       } else {
         return file;
