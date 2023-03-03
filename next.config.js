7523dde76b85f1a55eafc6d6efe1ad30fe6bad74
happo31 <happo31@gmail.com>
2023-03-04 00:11:28 +0900

    Revert "fix: copy-webpack-plugin を使う"
    This reverts commit f3107500b244c33411df57c53f1ad86220ed9fda.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
-const Copy = require('copy-webpack-plugin')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "dist",
-  webpack: (config) => {
-    config.plugins.push(new Copy({
-      patterns: [
-        process.env['SQLITE_DB_NAME']
-      ]
-    }));
-
-    return config;
-  }
+  distDir: "dist"
 }
 
 module.exports = nextConfig
