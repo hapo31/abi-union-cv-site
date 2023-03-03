00309df48250a17cd295290364a5fb64088b275a
happo31 <happo31@gmail.com>
2023-03-04 00:11:35 +0900

    Revert "fix: 調査"
    This reverts commit 01817e4a3be6a081257c84c4950bb886ec795105.

--- src/pages/api/cv.ts
+++ src/pages/api/cv.ts
@@ -6,7 +6,6 @@ import {
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as t from "io-ts";
 import all from "@/apiUtil/db";
-import { readdirSync } from "fs";
 
 const validateQuery = t.keyof({
   arknights: null,
@@ -20,57 +19,53 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.json({ dir: readdirSync(process.cwd()).join(" ") });
+  const databaseQuery = req.query["database"];
 
-  return;
+  const targetTables = ["arknights", "bluearchive", "imas_cinderella"];
 
-  // const databaseQuery = req.query["database"];
+  const filterTableNames = Array.isArray(databaseQuery)
+    ? databaseQuery
+    : databaseQuery?.split(",") ?? [];
 
-  // const targetTables = ["arknights", "bluearchive", "imas_cinderella"];
+  if (!isValidOrReponseError(res, t.array(validateQuery), filterTableNames)) {
+    return;
+  }
 
-  // const filterTableNames = Array.isArray(databaseQuery)
-  //   ? databaseQuery
-  //   : databaseQuery?.split(",") ?? [];
+  try {
+    const selectCharaAliases = targetTables
+      .map(
+        (table) =>
+          ` ${table}.cv_name as ${table}_cv_name, ${table}.cv_name_read as ${table}_cv_name_read, ${table}.id as ${table}_id, ${table}.chara as ${table}_chara`
+      )
+      .join(", ");
+    const joinOnPlaceHolder = targetTables
+      .filter((_, i) => i >= 1)
+      .map(
+        (table) =>
+          `left outer join ${table} on ${targetTables[0]}.cv_name = ${table}.cv_name`
+      )
+      .join(" ");
 
-  // if (!isValidOrReponseError(res, t.array(validateQuery), filterTableNames)) {
-  //   return;
-  // }
+    const filterPlaceHolder = filterTableNames
+      .map((table) => `${table}_chara is not null`)
+      .join(" and ");
 
-  // try {
-  //   const selectCharaAliases = targetTables
-  //     .map(
-  //       (table) =>
-  //         ` ${table}.cv_name as ${table}_cv_name, ${table}.cv_name_read as ${table}_cv_name_read, ${table}.id as ${table}_id, ${table}.chara as ${table}_chara`
-  //     )
-  //     .join(", ");
-  //   const joinOnPlaceHolder = targetTables
-  //     .filter((_, i) => i >= 1)
-  //     .map(
-  //       (table) =>
-  //         `left outer join ${table} on ${targetTables[0]}.cv_name = ${table}.cv_name`
-  //     )
-  //     .join(" ");
-
-  //   const filterPlaceHolder = filterTableNames
-  //     .map((table) => `${table}_chara is not null`)
-  //     .join(" and ");
-
-  //   if (filterTableNames.length === 0) {
-  //     const sql = `select ${selectCharaAliases} from ${targetTables[0]} ${joinOnPlaceHolder}`;
-  //     const rows = await all(sql);
-  //     responseOk(res, rows);
-  //   } else {
-  //     const sql = `select ${filterTableNames[0]}.cv_name, ${filterTableNames[0]}.cv_name_read, ${selectCharaAliases} from ${targetTables[0]} ${joinOnPlaceHolder} where ${filterPlaceHolder}`;
-  //     const rows = await all(sql);
-  //     responseOk(res, rows);
-  //   }
-  // } catch (e) {
-  //   if (isError(e)) {
-  //     responseError(res, "InternalServerError", e.message, e);
-  //   } else {
-  //     responseError(res, "InternalServerError", "Unknwon error", e);
-  //   }
-  // }
+    if (filterTableNames.length === 0) {
+      const sql = `select ${selectCharaAliases} from ${targetTables[0]} ${joinOnPlaceHolder}`;
+      const rows = await all(sql);
+      responseOk(res, rows);
+    } else {
+      const sql = `select ${filterTableNames[0]}.cv_name, ${filterTableNames[0]}.cv_name_read, ${selectCharaAliases} from ${targetTables[0]} ${joinOnPlaceHolder} where ${filterPlaceHolder}`;
+      const rows = await all(sql);
+      responseOk(res, rows);
+    }
+  } catch (e) {
+    if (isError(e)) {
+      responseError(res, "InternalServerError", e.message, e);
+    } else {
+      responseError(res, "InternalServerError", "Unknwon error", e);
+    }
+  }
 }
 
 function isError(e: any): e is Error {
