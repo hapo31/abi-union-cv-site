2d2116f4ea369a9d94db09d009f182679306dcd3
happo31 <happo31@gmail.com>
2023-05-27 21:42:37 +0900

    feat: 取得データをDBに放り込む

--- bin/scrape/index.ts
+++ bin/scrape/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import arknightsIndex from "./arknights";
-import sqlite3 from "sqlite3";
+import sqlite3, { Database } from "sqlite3";
+import { execQuery } from "./utils";
 
 dotenv.config();
 
@@ -8,5 +9,19 @@ dotenv.config();
   const sqlite = sqlite3.verbose();
   const db = new sqlite.Database(process.env["SQLITE_DB_NAME"] ?? "cv.db");
   const arknightsList = await arknightsIndex(db);
-  console.log(arknightsList);
+
+  const placeholder = arknightsList
+    .map((_, i) => `($cv_name${i}, $cv_name_read${i}, $chara${i})`)
+    .join(",");
+  await execQuery(
+    db,
+    `replace into arknights (cv_name, cv_name_read, chara) values ${placeholder}`,
+    arknightsList
+      .map((record, i) => ({
+        [`$cv_name${i}`]: record.cv_name,
+        [`$cv_name_read${i}`]: record.cv_name_read,
+        [`$chara${i}`]: record.chara,
+      }))
+      .reduce((acc, curr) => ({ ...acc, ...curr }), {})
+  );
 })();
